efa8bc840110e8213dcb7bf5664217b4966fb551
unknown <marlon_tancredo@hotmail.com>
2023-09-18 22:28:29 +0100

    feat: implementing home component

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,7 +1,9 @@
 const Home: React.FC = () => {
     return (
         <>
-            Home!
+            <p>Welcome to the home of the justice department of fakelandia.
+                <br /> Here you can browse a list of recent misdemeanours committed by our citzens, or you can confess to your own misdemeanour.
+            </p>
         </>
     )
 }
