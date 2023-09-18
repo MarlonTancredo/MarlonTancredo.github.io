f2490e0db79d7a9fc6aafcdcc08d46b441722465
unknown <marlon_tancredo@hotmail.com>
2023-09-18 22:28:53 +0100

    feat: implementing router

--- client/src/components/router/Router.tsx
+++ client/src/components/router/Router.tsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../home/Home"
+import MainLayout from "../layouts/MainLayout"
 import NotFound from "../not-found/NotFound"
 
 const Router = () =>
     <Routes>
-        <Route index element={<Home />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+        </Route>
     </Routes>
 
 export default Router
