794ce5a71c2af091c16d2ca27b63587c54434a93
unknown <marlon_tancredo@hotmail.com>
2023-09-22 17:59:22 +0100

    feat: styling app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter } from "react-router-dom"
 import Router from "./components/router/Router"
+import "./App.css"
 
 
 function App() {
   return (
-    <>
+    <div className="container">
       <BrowserRouter>
         <Router />
       </BrowserRouter>
-    </>
+    </div>
   )
 
 }
