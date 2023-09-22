7b877cda74a3eae4a971aa51ce557f70e2d56a55
unknown <marlon_tancredo@hotmail.com>
2023-09-22 18:06:54 +0100

    feat: adding pages to router

--- client/src/components/router/Router.tsx
+++ client/src/components/router/Router.tsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from "react-router-dom"
-import Home from "../home/Home"
+import Home from "../../pages/home/Home"
 import MainLayout from "../layouts/MainLayout"
+import Misdemeanours from "../../pages/misdemeanours/Misdemeanors"
+import ConfessToUs from "../../pages/confess-to-us/ConfessToUs"
 import NotFound from "../not-found/NotFound"
+
 
 const Router = () =>
     <Routes>
         <Route path="/" element={<MainLayout />}>
             <Route index element={<Home />} />
+            <Route path="misdemeanours" element={<Misdemeanours />} />
+            <Route path="confess-to-us" element={<ConfessToUs />} />
             <Route path="*" element={<NotFound />} />
         </Route>
     </Routes>
