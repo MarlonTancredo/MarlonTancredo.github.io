95ccbae62b6cfc1aa87f4d01dfc937e2b6f026f9
unknown <marlon_tancredo@hotmail.com>
2023-09-22 18:04:55 +0100

    feat: styling navbar.tsx

--- client/src/components/header/NavBar.tsx
+++ client/src/components/header/NavBar.tsx
@@ -4,11 +4,11 @@ import { NavLink } from "react-router-dom"
 const NavBar = () => {
     return (
         <>
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/misdemeanours">Misdemeanours</NavLink></li>
-                <li><NavLink to="/confess-to-us">Confess to us</NavLink></li>
-            </ul>
+            <ul className="navbar__container">
+                <li className="navbar__item"><NavLink to="/" className="navbar__link">Home</NavLink></li>
+                <li className="navbar__item"><NavLink to="/misdemeanours" className="navbar__link">Misdemeanours</NavLink></li>
+                <li className="navbar__item"><NavLink to="/confess-to-us" className="navbar__link">Confess to us</NavLink></li>
+            </ul >
         </>
     )
 }
