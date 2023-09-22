4022fb3b54497c8ffd47fa01ff876e836efcf13a
unknown <marlon_tancredo@hotmail.com>
2023-09-22 18:04:15 +0100

    feat: styling header.tsx

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,8 +1,12 @@
+import Title from "./Title"
 import NavBar from "./NavBar"
 
 const Header = () => {
     return (
-        <NavBar />
+        <header style={{ display: "flex", border: "1px solid black", alignItems: "center", justifyContent: "center" }} >
+            <Title />
+            <NavBar />
+        </header >
     )
 }
 
