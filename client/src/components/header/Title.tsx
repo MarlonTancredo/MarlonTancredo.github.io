d83c3e2a62fa69e1ae8ef8000306ca334ec93a96
unknown <marlon_tancredo@hotmail.com>
2023-09-22 18:10:08 +0100

    styles: styling header

---
+++ client/src/components/header/Title.tsx
@@ -0,0 +1,13 @@
+import "./styles.css"
+
+const Title = () => {
+    return (
+        <div className="title__container">
+            <p>FAKELANDIA</p>
+            <p>JUSTICE</p>
+            <p>DEPARTMENT</p>
+        </div>
+    )
+}
+
+export default Title
