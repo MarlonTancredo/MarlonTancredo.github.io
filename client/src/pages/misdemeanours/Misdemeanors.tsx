1074099bc2b680428f5c32396c99f7c316d0640f
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-10-03 11:44:17 +0100

    feat: importing useMisdemanour hook

--- client/src/pages/misdemeanours/Misdemeanors.tsx
+++ client/src/pages/misdemeanours/Misdemeanors.tsx
@@ -1,7 +1,14 @@
+import useMisdemeanour from "../../hooks/useMisdemeanour";
+
 const Misdemeanours = () => {
+    const [data] = useMisdemeanour("http://localhost:8080/api/misdemeanours/1");
+    //console.log(data);
+
     return (
         <>
-            <h1>Misdemeanours</h1>
+            {data?.map((element: unknown) => {
+                console.log(element);
+            })}
         </>
     );
 };
