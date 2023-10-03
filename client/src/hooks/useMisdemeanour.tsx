cfe74f210b7d6ef2df9b16091ccf8e3dd7995587
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-10-03 11:43:26 +0100

    feat: implmenting useMisdemanour hook

---
+++ client/src/hooks/useMisdemeanour.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+type Misdemeanours = {
+    citizenId: number;
+    misdemeanour: string;
+    date: string;
+};
+
+const useMisdemeanour = (url: string) => {
+    const [data, setData] = useState<Misdemeanours[]>();
+
+    useEffect(() => {
+        const fetchMisdemeanour = async () => {
+            const response = await fetch(url);
+            const misdemeanoursData = await response.json();
+            setData(misdemeanoursData.misdemeanours);
+        };
+        fetchMisdemeanour();
+    }, [url]);
+
+    return [data];
+};
+
+export default useMisdemeanour;
