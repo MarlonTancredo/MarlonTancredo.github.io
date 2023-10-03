6790465e5f513fb9ea23db31097f8fafd0bcdaf6
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-10-03 15:08:39 +0100

    feat: rendering images

--- client/src/pages/misdemeanours/Misdemeanors.tsx
+++ client/src/pages/misdemeanours/Misdemeanors.tsx
@@ -4,26 +4,28 @@ const Misdemeanours = () => {
     const [data] = useMisdemeanour("http://localhost:8080/api/misdemeanours/5");
 
     return (
-        <>
-            <table>
-                <thead>
+        <table style={{ textAlign: "center" }}>
+            <thead>
+                <tr>
+                    <th>Citizen Id</th>
+                    <th>Date</th>
+                    <th>Misdemeanour</th>
+                    <th>Punishment Idea</th>
+                </tr>
+            </thead>
+            {data?.map(({ citizenId, date, misdemeanour }, id: number) => (
+                <tbody key={citizenId}>
                     <tr>
-                        <th>citizenId</th>
-                        <th>date</th>
-                        <th>misdemeanour</th>
+                        <td>{citizenId}</td>
+                        <td>{date}</td>
+                        <td>{misdemeanour}</td>
+                        <td>
+                            <img src={`https://picsum.photos/id/${id}/50/50`} />
+                        </td>
                     </tr>
-                </thead>
-                {data?.map(({ citizenId, date, misdemeanour }) => (
-                    <tbody key={citizenId}>
-                        <tr>
-                            <td>{citizenId}</td>
-                            <td>{date}</td>
-                            <td>{misdemeanour}</td>
-                        </tr>
-                    </tbody>
-                ))}
-            </table>
-        </>
+                </tbody>
+            ))}
+        </table>
     );
 };
 
