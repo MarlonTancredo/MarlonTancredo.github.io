fc611d142c2ee6a99df52d532e99af9dcfa0c17a
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-10-03 12:11:11 +0100

    feat: rendering misdemeanours

--- client/src/pages/misdemeanours/Misdemeanors.tsx
+++ client/src/pages/misdemeanours/Misdemeanors.tsx
@@ -1,14 +1,28 @@
 import useMisdemeanour from "../../hooks/useMisdemeanour";
 
 const Misdemeanours = () => {
-    const [data] = useMisdemeanour("http://localhost:8080/api/misdemeanours/1");
-    //console.log(data);
+    const [data] = useMisdemeanour("http://localhost:8080/api/misdemeanours/5");
 
     return (
         <>
-            {data?.map((element: unknown) => {
-                console.log(element);
-            })}
+            <table>
+                <thead>
+                    <tr>
+                        <th>citizenId</th>
+                        <th>date</th>
+                        <th>misdemeanour</th>
+                    </tr>
+                </thead>
+                {data?.map(({ citizenId, date, misdemeanour }) => (
+                    <tbody key={citizenId}>
+                        <tr>
+                            <td>{citizenId}</td>
+                            <td>{date}</td>
+                            <td>{misdemeanour}</td>
+                        </tr>
+                    </tbody>
+                ))}
+            </table>
         </>
     );
 };
