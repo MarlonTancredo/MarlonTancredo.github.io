ddfbf1fdfb561da48863c94334fe13b048f4b550
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-10-05 12:33:20 +0100

    feat: implementing loading logic

--- client/src/pages/misdemeanours/Misdemeanors.tsx
+++ client/src/pages/misdemeanours/Misdemeanors.tsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import useMisdemeanour from "../../hooks/useMisdemeanour";
 
 const Misdemeanours = () => {
-    const [data] = useMisdemeanour("http://localhost:8080/api/misdemeanours/4");
+    const data = useMisdemeanour("http://localhost:8080/api/misdemeanours/4");
+    const [loading] = useState("Loading...");
+
+    if (!data) {
+        return (
+            <table style={{ textAlign: "center" }}>
+                <thead>
+                    <tr>
+                        <th>Citizen Id</th>
+                        <th>Date</th>
+                        <th>Misdemeanour</th>
+                        <th>Punishment Idea</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{loading}</td>
+                        <td>{loading}</td>
+                        <td>{loading}</td>
+                        <td>{loading}</td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
 
     return (
         <table style={{ textAlign: "center" }}>
