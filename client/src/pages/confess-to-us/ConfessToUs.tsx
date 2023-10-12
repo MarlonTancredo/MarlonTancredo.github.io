3590681f4a6fd5675f0b44839c0690ca43b8c3bc
unknown <marlon_tancredo@hotmail.com>
2023-10-12 11:51:05 +0100

    feat: implementing paragraph of confess to us page

--- client/src/pages/confess-to-us/ConfessToUs.tsx
+++ client/src/pages/confess-to-us/ConfessToUs.tsx
@@ -1,7 +1,16 @@
 const ConfessToUs = () => {
     return (
         <>
-            <h1>Confess to us</h1>
+            <p>
+                It's very difficult to catch people committing misdemeanours so
+                <br />
+                we appreciate it when citizens confess to us directly.
+                <br />
+                <br />
+                However, if you're just having a hard day and need to vent then
+                <br />
+                you're welcome to contact us here too. Up to you!
+            </p>
         </>
     );
 };
